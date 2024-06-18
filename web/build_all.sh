1278c7ab93abbecf8ff6a4b98edeee0a771e7eb6
Leon W. <leonwiemers@gmail.com>
2024-06-18 15:52:55 +0200

    fix deploy

--- web/build_all.sh
+++ web/build_all.sh
@@ -1,17 +1,17 @@
-BUILD_DIR="__build__"
+BUILD_DIR_NAME="__build__"
 
-mkdir "$BUILD_DIR"
+mkdir "$BUILD_DIR_NAME"
 
 # countdown
-mkdir "$BUILD_DIR/countdown"
+mkdir "$BUILD_DIR_NAME/countdown"
 cd countdown
 npm ci
 npm run build
-cp -r build/. "$BUILD_DIR/countdown"
+cp -r build/. "../$BUILD_DIR_NAME/countdown"
 cd ..
 
 # helloworld
-mkdir "$BUILD_DIR/helloworld"
+mkdir "$BUILD_DIR_NAME/helloworld"
 cd helloworld
-cp helloworld.html "$BUILD_DIR/helloworld/index.html"
+cp helloworld.html "../$BUILD_DIR_NAME/helloworld/index.html"
 cd ..
