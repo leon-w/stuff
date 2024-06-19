39557fa7a6d8d5bdcb7ee97145d560a05cc71ed5
Leon W. <leonwiemers@gmail.com>
2024-06-20 00:30:05 +0200

    add download all files snippet

---
+++ browser_snippets/download_all_files.js
@@ -0,0 +1,24 @@
+function downloadAllFiles(filetype = ".pdf", numerate = true) {
+    const urls = Array.from(
+        new Set(
+            document
+                .querySelectorAll("a")
+                .map(a => a.href)
+                .filter(url => url.endsWith(filetype))
+        )
+    );
+
+    if (numerate) {
+        const digits = Math.ceil(Math.log10(urls.length));
+        const commands = [];
+        for (let i = 0; i < urls.length; i++) {
+            const url = urls[i];
+            const num = i.toString().padStart(digits, "0");
+            const fileName = url.split("/").pop();
+            commands.push(`wget -O ${num}_${fileName} ${url}`);
+        }
+        return commands.join(" && ");
+    } else {
+        return `wget ${urls.join(" ")}`;
+    }
+}
